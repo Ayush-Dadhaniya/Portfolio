76d09df676b9c859b3fa618b8510d7968fbe1e33
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-04-27 16:38:07 +0530

    Experience page section added

--- portfolio_ayush_dadhaniya/components/Experience/page.js
+++ portfolio_ayush_dadhaniya/components/Experience/page.js
@@ -13,9 +13,6 @@ const Experience = () => {
 
         {/* Experience Card */}
         <div className="flex flex-col sm:flex-row justify-start sm:items-start mb-20 relative z-10">
-          <div className="w-6 h-6 bg-white rounded-full shadow-lg flex items-center justify-center">
-            <span className="text-black font-bold text-lg">1</span>
-          </div>
           <div className="ml-4 sm:ml-8 bg-gradient-to-r from-pink-500 to-indigo-500 p-8 rounded-lg shadow-lg w-full sm:w-2/3 transform hover:scale-105 transition-transform duration-500 ease-in-out">
             <h3 className="text-3xl sm:text-4xl font-serif font-bold mb-4">System Engineer</h3>
             <h3 className="text-2xl sm:text-3xl text-black font-sans font-bold mb-1">CEREBULB</h3>
