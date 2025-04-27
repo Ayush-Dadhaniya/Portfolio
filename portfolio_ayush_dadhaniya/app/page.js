b48fdee5e3a3ef388f9e209cb07f03318f8ed5e6
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-04-27 13:53:02 +0530

    Added Education

--- portfolio_ayush_dadhaniya/app/page.js
+++ portfolio_ayush_dadhaniya/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '@/Navbar/page';
 import About from '@/About/page';
+import Education from '@/Education/page';
 export default function Home() {
   return (
     <div className="bg-black h-fit">
       <Navbar />
-      <About />
+      <section id="about" className="h-full"><About/></section>
+      <section id="education" className="h-full"><Education/></section>
     </div>
   );
 }
