b3fcc4847c065649623520c8e783e9e3d7f1dbb0
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-04-27 16:35:31 +0530

    Experience section added

--- portfolio_ayush_dadhaniya/app/page.js
+++ portfolio_ayush_dadhaniya/app/page.js
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar/page';
 import About from '@/components/About/page';
 import Education from '@/components/Education/page';
 import Skills from '@/components/Skills/page';
+import Experience from '@/components/Experience/page';
 export default function Home() {
   return (
     <div className="bg-black h-fit">
@@ -9,6 +10,7 @@ export default function Home() {
       <section id="about" className="h-full"><About/></section>
       <section id="education" className="h-full"><Education/></section>
       <section id="skills" className="h-full"><Skills/></section>
+      <section id="experience" className="h-full"><Experience/></section>
     </div>
   );
 }
