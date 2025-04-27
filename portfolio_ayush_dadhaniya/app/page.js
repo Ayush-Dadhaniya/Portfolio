fde2032247bfc37a754ad4bde279881c9596c4cc
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-04-27 15:25:27 +0530

    Added skills section

--- portfolio_ayush_dadhaniya/app/page.js
+++ portfolio_ayush_dadhaniya/app/page.js
@@ -1,12 +1,14 @@
-import Navbar from '@/Navbar/page';
-import About from '@/About/page';
-import Education from '@/Education/page';
+import Navbar from '@/components/Navbar/page';
+import About from '@/components/About/page';
+import Education from '@/components/Education/page';
+import Skills from '@/components/Skills/page';
 export default function Home() {
   return (
     <div className="bg-black h-fit">
       <Navbar />
       <section id="about" className="h-full"><About/></section>
       <section id="education" className="h-full"><Education/></section>
+      <section id="skills" className="h-full"><Skills/></section>
     </div>
   );
 }
