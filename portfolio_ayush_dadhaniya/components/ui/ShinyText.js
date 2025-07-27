46e9c61d414d68f7ca8f6decb13577ce5ceba9a3
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-07-27 23:38:11 +0530

    Added responsivness for the mobile view 5

--- portfolio_ayush_dadhaniya/components/ui/ShinyText.js
+++ portfolio_ayush_dadhaniya/components/ui/ShinyText.js
@@ -3,7 +3,7 @@ const ShinyText = ({ text, disabled = false, speed = 15, className = '' }) => {
   
     return (
       <div
-        className={`text-transparent text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold bg-clip-text inline-block overflow-hidden ${disabled ? '' : 'animate-shine'} ${className}`}
+        className={`text-transparent text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold bg-clip-text inline-block overflow-hidden break-words ${disabled ? '' : 'animate-shine'} ${className}`}
         style={{
           backgroundImage: 'linear-gradient(120deg, #18181b 40%, #fff 50%, #18181b 60%)',
           backgroundSize: '200% 100%',
